d9928fa108f2db8194a15c73d7a8993105bd2ed7
Diego Braun <diegobraun2000@gmail.com>
2019-02-08 23:24:18 -0200

    Exemplo de null delegates

---
+++ NullDelegates/NullDelegates/Framework.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace NullDelegates
+{
+    //public class Boleto
+    //{
+    //    public void Pagar(double valor)
+    //    {
+    //        WriteLine($"Pago valor de {valor} no boleto");
+    //    }
+    //}
+    public delegate void PagarEvent (double valor);
+
+    public class Pedido
+    {
+        public event PagarEvent Pagar;
+        //Boleto boleto = new Boleto();
+        public void Fechar(double valor)
+        {
+            //delegação
+            //if (Pagar != null)
+            //    Pagar(valor);
+            //else
+            //    WriteLine("Não existem formas de pagamento!");
+            Pagar?.Invoke(valor);
+        }
+
+
+    }
+}
---
+++ NullDelegates/NullDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullDelegates
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe valor do pedido: ");
+            var valor = Convert.ToDouble(Console.ReadLine());
+            var pedido = new Pedido();
+            pedido.Pagar += v => Console.WriteLine($"Pago valor de {v} no boleto");
+            pedido.Fechar(valor);
+            Console.ReadLine();
+        }
+
+        //private static void Pedido_Pagar(double valor)
+        //{
+        //    Console.WriteLine($"Pago valor de {valor} no boleto");
+        //}
+    }
+}
