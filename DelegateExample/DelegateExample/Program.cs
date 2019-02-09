fc65c132e6f552b3699c35273d669c877159d8aa
Diego Braun <diegobraun2000@gmail.com>
2019-02-08 22:54:51 -0200

    Exemplo de delegate

---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    class Program
+    {
+        public delegate void ExemploDelegate();
+
+        public static void Ex()
+        {
+            Console.WriteLine("Opa");
+        }
+
+        static void Main(string[] args)
+        {
+            ExemploDelegate x = new ExemploDelegate(Ex);
+            x.Invoke();
+            Console.ReadKey();
+        }
+    }
+}
