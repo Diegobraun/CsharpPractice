d3796a33b91907d2dcd88d66186dbd543a399e35
Diego Braun <diegobraun2000@gmail.com>
2019-02-09 00:24:06 -0200

    Exemplo de exception classes

---
+++ ExceptionClasses/ExceptionClasses/Conta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionClasses
+{
+    public class Conta
+    {
+        private double _saldo = 0;
+        private const double LIMITE = 500;
+
+        public void Depositar(double valor)
+        {
+            _saldo = _saldo + valor;
+        }
+        public void Sacar(double valor)
+        {
+            if (valor > LIMITE + _saldo)
+                throw new SaldoInsuficienteException("Saldo insuficiente!");
+                _saldo = _saldo - valor;
+        }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+    }
+
+    [Serializable]
+    public class SaldoInsuficienteException : Exception
+    {
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(string message) : base(message)
+        {
+        }
+
+        public SaldoInsuficienteException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SaldoInsuficienteException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
