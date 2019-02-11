49e5a1607e5cf7aaea7fa3ea02e25fa26348b41c
Diego Braun <diegobraun2000@gmail.com>
2019-02-11 20:50:46 -0200

    Event delegate Initializer

---
+++ EventDelegateInitializer/EventDelegateInitializer/Framework.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateInitializer
+{
+
+    //public class Boleto
+    //{
+    //    public void Pagar(double valor)
+    //    {
+    //        Console.WriteLine($"Pago valor de {valor} no boleto");
+    //    }
+    //}
+    //public delegate void PagarEvent(double valor);
+
+    public class Pedido
+    {
+        public string Cliente { get; set; }
+        public EventHandler<EventArgs> Pagar { get; set; }
+
+        public void Fechar(double valor)
+        {
+            // Boleto boleto = new Boleto();
+            //delegação com delegate
+            Pagar(this, new EventArgs());
+        }
+
+
+    }
+}
---
+++ EventDelegateInitializer/EventDelegateInitializer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor do pedido: ");
+            var valor = Convert.ToDouble(Console.ReadLine());
+            var pedido = new Pedido()
+            {
+                Cliente = "Diego",
+                Pagar = (s,e) => Console.WriteLine("Pago no cartão")
+            };
+            pedido.Fechar(valor);
+            Console.ReadLine();
+        }
+
+        private static void Pedido_Pagar(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Pago no boleto");
+        }
+    }
+}
