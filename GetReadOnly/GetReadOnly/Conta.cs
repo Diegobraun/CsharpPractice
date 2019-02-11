dc5e961d6b79ed8865bd48c516c0c31c5bb8263b
Diego Braun <diegobraun2000@gmail.com>
2019-02-11 20:27:03 -0200

    Get read only

---
+++ GetReadOnly/GetReadOnly/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetReadOnly
+{
+    public class Conta
+    {
+        //antes do csharp 6
+        public double Saldo { get;  private set; } = 1000;
+
+        public string Cliente { get; set; }
+
+        public void Sacar(double valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        //public double getSaldo()
+        //{
+        //    return Saldo;
+        //}
+
+    }
+}
---
+++ GetReadOnly/GetReadOnly/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Conta()
+            {
+                Cliente = "Diego"
+            };
+            c.Saldo = 1000;
+            Console.WriteLine($"Prezado {c.Cliente} seu saldo é {c.Saldo}");
+            Console.ReadKey();
+           
+        }
+    }
+}
