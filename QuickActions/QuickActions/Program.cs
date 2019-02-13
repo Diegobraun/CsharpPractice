10a77bdd22556dc84583f4d3a90a71ad1b8c5602
Diego Braun <diegobraun2000@gmail.com>
2019-02-12 23:13:21 -0200

    Quick actions

---
+++ QuickActions/QuickActions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System;
+
+namespace QuickActions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GerarSomas();
+            CriaObjetos();
+        }
+
+        private static void GerarSomas()
+        {
+            int X = 0;
+            int Y = 0;
+            SomaSim(X, Y);
+        }
+
+        private static void SomaSim(int x, int y)
+        {
+            int v = x + y;
+        }
+
+
+        private static void CriaObjetos()
+        {
+            #region CriacaoObjetos
+            var con = new SqlConnection();
+            var ds = new DataSet();
+            var sb = new StringBuilder();
+            #endregion
+        }
+    }
+}
