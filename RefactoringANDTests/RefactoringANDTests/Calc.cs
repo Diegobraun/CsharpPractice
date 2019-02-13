67000227dd0c27f27f914cbd1cd6f0c00ac31592
Diego Braun <diegobraun2000@gmail.com>
2019-02-13 00:45:48 -0200

    Refactoring and tests

---
+++ RefactoringANDTests/RefactoringANDTests/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RefactoringANDTests
+{
+    public class Calc : object, ICalc
+    {
+        public Calc()
+        {
+        }
+
+        public int Somar(int x, int y) => x + y;
+        public int Multiplicar(int x, int y) => x * y;
+    }
+}
---
+++ RefactoringANDTests/RefactoringANDTests/FrmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefactoringANDTests
+{
+    public partial class FrmMain : Form
+    {
+        public ICalc c = new Calc();
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var r = c.Somar(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            textBox3.Text = r.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var r = c.Multiplicar(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            textBox3.Text = r.ToString();
+        }
+    }
+}
