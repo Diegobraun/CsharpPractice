285f30e9ba5ab77b74537880b9fe72a92e080b38
Diego Braun <diegobraun2000@gmail.com>
2019-02-16 17:25:01 -0200

    Aplicação simples com entity framework

---
+++ AplicacaoEntityFramework/AplicacaoEntityFramework/ClientesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace AplicacaoEntityFramework
+{
+    class ClientesContext:DbContext
+    {
+        public ClientesContext() : base("BancoClientes")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ AplicacaoEntityFramework/AplicacaoEntityFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cli = new Cliente
+            {
+                Nome = "Diego",
+                Credito = 900,
+                Idade = 18
+            };
+            ClientesContext db = new ClientesContext();
+            db.Clientes.Add(cli);
+            db.SaveChanges();
+            Console.WriteLine("Exito");
+            Console.ReadKey();
+        }
+    }
+}
