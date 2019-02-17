e0bc582a49ec9982960a9037830712859d7671ab
Diego Braun <diegobraun2000@gmail.com>
2019-02-17 16:07:53 -0300

    Adição de tratamento de exceções

---
+++ ProjectPandP/TesteWhile/TratamentoExceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteWhile
+{
+    class TratamentoExceptions
+    {
+        public void ExceptionBasic(object obj)
+        {
+            try
+            {
+                int a;
+                int.TryParse(obj.ToString(), out a);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }   
+    }
+}
