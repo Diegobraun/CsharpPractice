c36b5ceba94c3edfc50049744a99c0b5cde10abc
Diego Braun <diegobraun2000@gmail.com>
2019-03-08 00:23:20 -0300

    Automatic properties

---
+++ AutomaticProperties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AutomaticProperties
+{
+    public class Funcionario{
+        public int Codigo { get; set;}
+        public string Nome { get; set;}
+        public double Salario { get; set;}
+
+        public Funcionario(){
+            Codigo = 0;
+            Nome = "";
+            Salario = 0;
+        }
+
+        public Funcionario(int codigo, string nome, double salario){
+            Codigo = codigo;
+            Nome = nome;
+            Salario = salario;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario(
+            123,
+            "Diego",
+            1000);
+            
+            Console.ReadLine();
+        }
+    }
+}
