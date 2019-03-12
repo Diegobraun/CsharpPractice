168d143a898bf8c250b8de5411fd160514bb8e5e
Diego Braun <diegobraun2000@gmail.com>
2019-03-12 10:16:12 -0300

    Example of inference types like var and dynamic

---
+++ InferenceTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InferenceTypes
+{
+    public class Connection{
+        public string ConnectionString;
+        public void Conectar(){
+            Console.WriteLine("Conectando");
+        }
+        public void Fechar(){
+            Console.WriteLine("Fechando conexão");
+        }
+    }
+
+    public class Fabrica{
+        public static Connection CriarConnection(){
+            return new Connection();
+        }
+    }
+    class Program{
+        static void Main(string[] args)
+        {
+            var con = Fabrica.CriarConnection();
+            con.Conectar();   
+
+            dynamic conn = Fabrica.CriarConnection();
+            conn.Conectar();         
+        }
+    }
+}
